c530fdf4efb9d341e36087fd55de179f648caadf
sppmao <sppmao@nus.edu.sg>
2021-01-18 19:12:42 +0800

    Update internal datasets

--- data-raw/url_list.R
+++
@@ -1,6 +0,0 @@
-url_list <- c(
-  "en" = "https://data.stats.gov.cn/english/easyquery.htm",
-  "zh" = "https://data.stats.gov.cn/easyquery.htm"
-)
-
-usethis::use_data(url_list, overwrite = TRUE, internal = TRUE)
